f4f37f00b4e3c98bf4607a0259baeb54b5a0a62d
Julian_Chu <walkingice0204@gmail.com>
2019-04-19 11:14:52 +0900

    add empty project

---
+++ app/src/main/java/net/julianchu/applinktest/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.julianchu.applinktest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
